90ef5743b270ab50bd633e86bec96fa83a47f210
DeepakS-Github <deepakanonymous75@gmail.com>
2024-05-18 17:09:02 +0530

    update: echartspie

--- src/components/common/EchartsPie.tsx
+++ src/components/common/EchartsPie.tsx
@@ -8,42 +8,40 @@ const EchartsPie: React.FC = () => {
         },
         series: [
             {
+
                 name: 'Dummy Data',
                 type: 'pie',
-                // radius: ['40%', '70%'],  // Set the inner and ourter radius of the pie chart to make it look like doughnut chart
+                radius: ['40%', '70%'],  // Set the inner and ourter radius of the pie chart to make it look like doughnut chart
                 data: [
-                    { value: 335, name: 'A' },
-                    { value: 310, name: 'B' },
-                    { value: 234, name: 'C' },
-                    { value: 135, name: 'D' },
-                    { value: 1548, name: 'E' },
-                    { value: 675, name: 'F' },
-                    { value: 848, name: 'G' },
-                    { value: 420, name: 'H' },
-                    { value: 530, name: 'I' },
-                    { value: 1200, name: 'J' },
-                    { value: 800, name: 'K' },
-                    { value: 700, name: 'L' },
-                    { value: 600, name: 'M' },
-                    { value: 500, name: 'N' },
-                    { value: 400, name: 'O' },
-                    { value: 300, name: 'P' },
-                    { value: 200, name: 'Q' },
-                    { value: 100, name: 'R' },
-                    { value: 50, name: 'S' },
-                    { value: 25, name: 'T' },
-                    { value: 10, name: 'U' },
-                    { value: 5, name: 'V' },
-                    { value: 3, name: 'W' },
-                    { value: 2, name: 'X' },
-                    { value: 1, name: 'Y' },
-                    { value: 1, name: 'Z' }
+                    { value: 335, name: 'A', head: 'Bounded' },
+                    { value: 310, name: 'B', head: 'Unbounded' },
+                    { value: 234, name: 'C', head: 'Unbounded' },
+                    { value: 135, name: 'D', head: 'Bounded' },
                 ],
                 label: {
                     show: true,
-                    formatter: '{b}: {d}%',
-                    textStyle: {
-                        color: 'white' // Set label text color to white
+                    formatter: function (params: any) {
+                        console.log(params);
+                        return `{a|${params.data.head}}\n{b|${params.name}:} {c|${params.value}}`;
+                    },
+                    rich: {
+                        a: {
+                            // color: function(params: any) {
+                            //     return params.color;
+                            // },
+                            color: 'white',
+                            fontSize: '14px',
+                            height: 30,
+                            fontWeight: 600,
+                        },
+                        b: {
+                            color: 'white', 
+                            fontSize: '12px',
+                        },
+                        c: {
+                            color: 'white', 
+                            fontSize: '12px',
+                        }
                     }
                 },
                 itemStyle: {
